51bfd97f68b1ce5399428d5f28d5febd97898520
Javier Angel Varela Cebey <gekito@gmail.com>
2019-06-13 20:07:20 +0200

    Initial Commit

---
+++ MovieDB/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MovieDB
+//
+//  Created by Javier Angel Varela Cebey on 13/06/2019.
+//  Copyright © 2019 Javier Angel Varela Cebey. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
